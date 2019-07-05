81f329c08928e5352e7d6f912b62674400f280c7
Lars Kellogg-Stedman <lars@oddbit.com>
2019-07-05 15:14:18 -0400

    initial import

---
+++ git_ptt/main.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+import configparser
+import logging
+import re
+
+import click
+import git
+
+LOG = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option('-h', '--header', default='X-branch-name')
+@click.option('-q', '--query', is_flag=True)
+@click.option('-d', '--delete', is_flag=True)
+@click.option('-v', '--verbose', count=True)
+@click.option('-c', '--continue', '_continue', is_flag=True)
+@click.option('-R', '--remote')
+@click.option('-s', '--since')
+@click.option('-p', '--prefix')
+@click.argument('revisions', nargs=-1)
+def main(header, delete, query, verbose, _continue, remote, since, prefix,
+         revisions):
+    try:
+        loglevel = ['WARNING', 'INFO', 'DEBUG'][verbose]
+    except IndexError:
+        loglevel = 'DEBUG'
+
+    logging.basicConfig(
+        level=loglevel,
+    )
+
+    repo = git.Repo()
+    conf = repo.config_reader()
+
+    if remote is None:
+        try:
+            remote = conf.get('ptt', 'remote')
+        except configparser.Error:
+            remote = 'origin'
+
+    rem = repo.remote(remote)
+
+    if since and revisions:
+        raise click.ClickException('you cannot use --since and provide '
+                                   'a list of revisions')
+
+    if since:
+        revisions = repo.iter_commits('{}..'.format(since))
+
+    targets = []
+    for rev in revisions if revisions else ['HEAD']:
+        com = repo.commit(rev)
+        LOG.info('checking commit %s', com.hexsha[:7])
+
+        match = re.search(r'{}: (?P<name>\S+)'.format(
+            header.lower()), com.message.lower())
+        if not match:
+            if _continue:
+                LOG.warning('No branch name in %s', com.hexsha[:7])
+                continue
+            else:
+                raise click.ClickException(
+                    'No branch name in {}'.format(com.hexsha[:7]))
+
+        target = match.group(1)
+        if prefix:
+            target = '{}{}'.format(prefix, target)
+        LOG.info('found branch name %s for commit %s', target, com.hexsha[:7])
+        targets.append((com, target))
+
+    for com, target in targets:
+        if query:
+            print('{}: {}'.format(com.hexsha[:7], target))
+        elif delete:
+            LOG.warning('deleting branch %s from remote %s', target, rem)
+            rem.push(':refs/heads/{}'.format(target))
+        else:
+            LOG.warning('pushing %s to branch %s on remote %s', com.hexsha[:7],
+                        target, rem)
+            res = rem.push('+{}:refs/heads/{}'.format(com.hexsha, target))
+            if res:
+                LOG.warning('res: %s', res)
